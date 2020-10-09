56dd10a4c612d4427ce206b4bebac72d1d265408
v_vjghuang <v_vjghuang@tencent.com>
2020-10-09 17:20:28 +0800

    add file -> FlyingObject.java

--- java/core/FlyingObject.java
+++ java/core/FlyingObject.java
@@ -1,4 +1,11 @@
 package core;
 
-public class FlyingObject {
+public abstract class FlyingObject {
+    protected int ySpeed;
+    protected int width;
+    protected int height;
+
+    public FlyingObject(int width, int height, int ySpeed){
+
+    }
 }
